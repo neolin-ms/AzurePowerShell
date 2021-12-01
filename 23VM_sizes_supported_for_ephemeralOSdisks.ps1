db1656d839d1e3007cb4cb70afd6e19e72ae36e4
Neo Lin <neo.lin@microsoft.com>
2021-12-01 10:58:25 +0800

    update the No.23 script

--- 23VM_sizes_supported_for_ephemeralOSdisks.ps1
+++ 23VM_sizes_supported_for_ephemeralOSdisks.ps1
@@ -1,3 +1,5 @@
+
+# Check VM sizes supported for ephemeral OS disks
 $vmSizes=Get-AzComputeResourceSku | where{$_.ResourceType -eq 'virtualMachines' -and $_.Locations.Contains('eastasia')} 
 
 foreach($vmSize in $vmSizes)
@@ -10,3 +12,9 @@ foreach($vmSize in $vmSizes)
        }
    }
 }
+
+# Check the Cache size
+##$vmSizes=Get-AzComputeResourceSku | where{$_.ResourceType -eq 'virtualMachines' -and $_.Name.Contains('Standard_M8ms') -and $_.Locations.Contains('eastasia')} 
+##$vmSizes=Get-AzComputeResourceSku | where{$_.ResourceType -eq 'virtualMachines' -and $_.Name.Contains('Standard_E4ds_v4') -and $_.Locations.Contains('eastasia')} 
+##$vmSizes=Get-AzComputeResourceSku | where{$_.ResourceType -eq 'virtualMachines' -and $_.Name.Contains('Standard_E8ds_v4') -and $_.Locations.Contains('eastasia')} 
+##$vmSizes.Capabilities
